fee1e7a80020d085b18ac1016886cc65b392e7bb
slindquist3 <slindquist3@gmail.com>
2016-07-16 21:24:32 -0400

    Completed ruby basics 001

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+var = "#{name}"
 end
 
-def argue
-  
+def argue(argument)
+return argument
 end
 
-def greeting
-
+def greeting(arg1, arg2)
+return arg1, arg2
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg = "cheese")
+  return "#{arg}"
 end
